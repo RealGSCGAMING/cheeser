d909c9b1bfc0514388bff7480d7ff739d2006248
RealGSCGAMING <129533800+RealGSCGAMING@users.noreply.github.com>
2026-05-20 10:50:05 -0700

    homepage

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-if (globalThis.location.href.includes("cheeserinc.com")) {
+if (globalThis.location.href.includes("cheeserinc.com") && !globalThis.location.href.includes("debug")) {
     globalThis.location.href = "maintenance.html";
+}
+
+if (globalThis.location.href.includes("skip")) {
+    document.getElementById("welcome-modal").style.display = "none";
 }
